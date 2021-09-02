267eaa03d740ada6b34d42da270ebae6260fba4b
augustouspedro <77211017+augustouspedro@users.noreply.github.com>
2021-09-02 10:01:18 -0300

    Update tftp.sh

--- tftp.sh
+++ tftp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ip=<device ip address>
-file=<name the firmware file or the directory> 
+ip="192.168.1.20"
+file="XW.v6.3.6.33330.210818.1930.bin" 
 
-echo "Ping teste"
+echo "Ping test"
 
 #ping test
 
-(( x = 5 ))
+(( x = 5 ))                                       
 (( y = 0 ))
-while [[ $x -ne 0 ]]; do
+while [[ $x -ne 0 ]]; do                         
 ping -c 1 $ip
 	rc=$?
 	if [[ $rc -eq 0 ]] ; then
@@ -19,35 +19,35 @@ ping -c 1 $ip
 		((x = x - 1)) 
 done 			
 
-			if [[ $y -ne 0 ]]; then
-				tftp $ip <<- EOF
-					bin
-					trace
-					put $file
-					exit
-					EOF
-						echo "Done"
-						echo "Wait until it ping again"
+if [[ $y -ne 0 ]]; then
+	tftp $ip <<- EOF
+		bin
+		trace
+		put $file
+		exit
+		EOF
+	echo "Done"
+	echo "Ping test"
 			
-				(( x = 20 ))	
-				(( z = 0 ))
-				while [[ $x -ne 0 ]]; do
-					ping -c 1 $ip	
-						rc=$?	
-						if [[ $rc -eq 0 ]]; then
-							(( x = 1 )) 
-			  				(( z = 1 ))		
-						fi
-							(( x = x - 1 ))              
-                		done
-						
-							if [[ $z -ne 0 ]]; then
-				       				echo "The device is ready!"
-			       				fi
-
-			else 
-				echo "Please, check the device connection"
+	(( x = 20 ))	
+	(( z = 0 ))
+	while [[ $x -ne 0 ]]; do
+		ping -c 1 $ip	
+		rc=$?	
+			if [[ $rc -eq 0 ]]; then
+				(( x = 1 )) 
+			  	(( z = 1 ))		
 			fi
+				(( x = x - 1 ))              
+        done
+						
+			if [[ $z -ne 0 ]]; then
+				echo "The device is ready!"
+			fi
+
+else 
+	echo "Please, check the device connection"
+fi
 
 
 		
